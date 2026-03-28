480f115db031265cb16ca4c982ddc11ebcbc58e9
Leon Si <leon@leonsilicon.com>
2026-03-28 15:31:36 -0400

    init

---
+++ readme.md
@@ -0,0 +1,7 @@
+# @keychord/chordpack-default
+
+The default chordpack that comes pre-installed with Chord.
+
+We use a chordpack instead of a chord package because we don't want to manually delegate all the app-scoped chords defined in `@keychord/chords-com.apple`.
+
+## Chord packages
---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import { config } from '@keychord/config'
+
+export default config({
+  vendor: [
+    "@keychord/chords-commands",
+    "@keychord/chords-os",
+    "@keychord/chords-menu",
+    "@keychord/chords-tray",
+    "@keychord/chords-chord",
+    "@keychord/chords-com.apple",
+  ]
+});
